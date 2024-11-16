92f635f3affd1159b6f4ffee9537907a37219577
Sandra Duqi <sandra.duqi@tesisquare.com>
2024-11-16 22:23:01 +0100

    Register form

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,29 +5,33 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext dataContext, ITokenService tokenService) : BaseApiController
+public class AccountController(DataContext dataContext, ITokenService tokenService, IMapper mapper) : BaseApiController
 {
 [HttpPost("register")]
 public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
  if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-   return Ok();
-    /*using var hmac= new HMACSHA512();
-    var user = new AppUser{
-    UserName = registerDto.Username.ToLower(),
-    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-    PasswordSalt = hmac.Key
-   };
+
+    using var hmac= new HMACSHA512();
+
+    var user = mapper.Map<AppUser>(registerDto); 
+    user.UserName = registerDto.Username.ToLower();
+    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+    user.PasswordSalt = hmac.Key;
+    
    dataContext.Users.Add(user);
    await dataContext.SaveChangesAsync();
+
    return new UserDto{
       Username = user.UserName,
-      Token = tokenService.CreateToken(user)
-   };*/
+      Token = tokenService.CreateToken(user),
+      KnownAs = user.KnownAs
+   };
 }
 
 [HttpPost("login")]
@@ -48,6 +52,7 @@ public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
    return new UserDto
    {
       Username = user.UserName,
+      KnownAs = user.KnownAs,
       Token = tokenService.CreateToken(user)  ,
       PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
    };
